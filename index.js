f166777966270d0ca6a7fd962a57cac5b52a4142
Tucker Bickler <tbickler@homeaway.com>
2019-02-15 23:55:37 -0600

    All the new stuff

--- index.js
+++ index.js
@@ -1,20 +1,55 @@
+const Hapi = require('hapi');
 const Twit = require('twit');
 const config = require('./config');
 
+const server = Hapi.server({port: 80});
+
+server.route([
+    {
+        method: 'GET',
+        path: '/webhook',
+        handler(request, h) {
+            console.log('REQUEST', request);
+            // const sha256_hash_digest = Hash.hmac(config.consumer_secret, )
+
+            // Hash.sha256().update()
+
+            // return {
+            //     response_token: `sha256=${crypto.createHmac('sha256', config.consumer_secret).update(crc_token).digest('base64')}`
+            // }
+        }
+    }
+    // {
+    //     method: 'POST',
+    //     path: '/webhook',
+    //     handler(request, h) {
+
+    //     }
+    // }
+]);
+
+
+
 const twit = new Twit(config);
 
-const userStream = twit.stream('user');
-userStream.on('follow', function({ source: {screen_name}}) {
-    console.log(`@${screen_name} followed you. Following back.`);
-    twit.post('friendships/create', {
-        screen_name,
-        follow: true
-    });
+twit.post(`/webhooks.json?url=https://${process.env.HEROKU_APP_NAME}.herokuapp.com/webhook`, function(err, data, response) {
+    console.log('RESPONSE', response);
 });
-userStream.on('unfollow', function({ source: {screen_name}}) {
-    console.log(`@${screen_name} unfollowed you. Unfollowing back.`);
-    twit.post('friendships/create', {
-        screen_name,
-        follow: false
-    });
-});
+
+
+
+// const userStream = twit.stream('user');
+// userStream.on('follow', function({ source: {screen_name}}) {
+//     console.log(`@${screen_name} followed you. Following back.`);
+//     twit.post('friendships/create', {
+//         screen_name,
+//         follow: true
+//     });
+// });
+// userStream.on('unfollow', function({ source: {screen_name}}) {
+//     console.log(`@${screen_name} unfollowed you. Unfollowing back.`);
+//     twit.post('friendships/create', {
+//         screen_name,
+//         follow: false
+//     });
+// });
